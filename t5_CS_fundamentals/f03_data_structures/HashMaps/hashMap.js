8c8075627a8c8c39a3f167652c4e4751d2bf0b2e
Andy Ly <andywindsong@gmail.com>
2019-01-16 22:06:31 -0800

    bit more on hash

--- t5_CS_fundamentals/f03_data_structures/HashMaps/hashMap.js
+++ t5_CS_fundamentals/f03_data_structures/HashMaps/hashMap.js
@@ -179,3 +179,38 @@ function anagrams (array) {
 /*
  * 3. Write a hash map implementation which uses separate chaining.
  */
+class HashTable {
+    // default bucket 42
+    constructor (size = 42) {
+        this.buckets = new Array(size);
+        this.size = size;
+    }
+    // hash function
+    hash (key) {
+        return key.toString().length % this.size;
+    }
+    // set function to add new data to hash table
+    set (key, value) {
+        let index = this.hash(key);
+        if (!this.buckets[index]){
+            this.buckets[index] = [];
+        }
+        this.buckets[index].push([key, value]);
+        return index;
+    }
+    // get method: helps get data by using key
+    get (key) {
+        // index of bucket
+        let index = this.hash(key);
+        // if there is no bucket
+        if (!this.buckets[index]) {
+            return null;
+        }
+        for (let bucket of this.buckets[index]) {
+            // if key matches
+            if (bucket[0] === key) {
+                return bucket[1]
+            }
+        }
+    }
+}
